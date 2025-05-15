fc960959afad715b36e45ba486487b54971e24d6
matteopere <matteo2005tito@gmail.com>
2025-05-15 17:30:41 +0200

    Update renderHtml.ts

--- src/renderHtml.ts
+++ src/renderHtml.ts
@@ -7,10 +7,11 @@ export function renderHtml(content: string) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Festa di paese – Scontrini</title>
         <link rel="stylesheet" href="https://static.integrations.cloudflare.com/styles.css">
-        <img src="logo.png">
+        
       </head>
       <body>
         <header>
+        <img src="logo.png">
           <h1>📑 Elenco Scontrini</h1>
         </header>
         <main>
